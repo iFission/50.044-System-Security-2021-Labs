aefe5e46351e2363b72964209a75c2bc1e579a8d
sheikhshack <redfreak97@gmail.com>
2021-07-01 16:49:12 -0400

    feat: done ex5 - with caveats

--- lab2_priv_separation/chroot-setup.sh
+++ lab2_priv_separation/chroot-setup.sh
@@ -57,6 +57,8 @@ mkdir -p /jail/usr/share/zoneinfo
 cp -r /usr/share/zoneinfo/America /jail/usr/share/zoneinfo/
 
 create_socket_dir /jail/echosvc 61010:61010 755
+create_socket_dir /jail/authsvc 61013:61013 755
+
 
 mkdir -p /jail/tmp
 chmod a+rwxt /jail/tmp
@@ -69,14 +71,22 @@ rm -rf /jail/zoobar/db
 
 python /jail/zoobar/zoodb.py init-person
 python /jail/zoobar/zoodb.py init-transfer
+python /jail/zoobar/zoodb.py init-cred
 
-set_perms 61012:61012 755 /jail/zoobar/db/person
-set_perms 61012:61012 755 /jail/zoobar/db/person/person.db
+# HOTFIX, to make it writeable by the 
+set_perms 61012:61012 777 /jail/zoobar/db/person
+set_perms 61012:61012 777 /jail/zoobar/db/person/person.db
 
 set_perms 61012:61012 755 /jail/zoobar/db/transfer
 set_perms 61012:61012 755 /jail/zoobar/db/transfer/transfer.db
-
+set_perms 61013:61013 700 /jail/zoobar/db/cred
+set_perms 61013:61013 700 /jail/zoobar/db/cred/cred.db
 
 # Hotfix to make echo server work now
 set_perms 61010:61010 755 /jail/zoobar/echo-server.py
+
+# For part 5 -- Auth service
+set_perms 61013:61013 755 /jail/zoobar/auth-server.py
+
+
 
